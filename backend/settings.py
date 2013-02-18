842d0f04c05c25d9668c0d64259d8d444b9fc4a4
Oscarbachtiar759 <oscarbachtiar759@gmail.com>
2013-02-17 23:02:24 -0800

    changed template dir once again

--- backend/settings.py
+++ backend/settings.py
@@ -110,7 +110,8 @@ ROOT_URLCONF = 'backend.urls'
 WSGI_APPLICATION = 'backend.wsgi.application'
 
 TEMPLATE_DIRS = (
-    "/apps/backend/templates",
+    #"/apps/backend/templates",
+    "os.path.join(os.path.abspath(os.path.curdir),'../backend/templates')"
     #"C:/Users/Oscar/Documents/School/Spring 2013/CS 169/warmup/backend/templates",
     
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
